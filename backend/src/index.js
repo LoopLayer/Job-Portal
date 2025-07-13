c81b3bb38e671caff5b0f82e8a430e185fbd8d6d
Kishan Sisodiya <KishanSisodiya.work@gmail.com>
2025-07-13 13:49:17 +0530

    Update User Routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
+import userRoute from "./routes/user.routes.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
