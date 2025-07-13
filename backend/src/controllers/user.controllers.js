1a4d577a49b4d149e426761b707009d86726399c
Kishan Sisodiya <KishanSisodiya.work@gmail.com>
2025-07-13 14:26:19 +0530

    Update user.controllers.js

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.models.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import getDataUri from "../utils/datauri.js";
+import getDataUri from "../utils/DataUri.js";
 import cloudinary from "../utils/cloudinary.js";
 
 export const register = async (req, res) => {
